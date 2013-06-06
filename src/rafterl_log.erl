7c20519a508e279ae00c0e35aec97f1fd2f32731
Eric Moritz <eric@themoritzfamily.com>
2013-06-06 13:52:05 -0400

    Added some metadata accessors

--- src/rafterl_log.erl
+++ src/rafterl_log.erl
@@ -1,3 +1,4 @@
+%%% -*- erlang -*-
 %%% @author Eric Moritz <eric@themoritzfamily.com>
 %%% @copyright (C) 2013, Eric Moritz
 %%% @doc
@@ -7,14 +8,23 @@
 
 -module(rafterl_log).
 
--export([new/1, last_index/1, append_entry/3, entry/2]).
+-export([new/1, 
+         % log ops
+         last_index/1, append_entry/3, entry/2, 
 
--define(INDEX_KEY, <<"last_index">>).
+         % metadata
+         voted_for/1, voted_for/2,
+         term/1, term/2
+        ]).
+
+-define(METADATA_KEY, <<"metadata">>).
+-define(INDEX_KEY, last_index).
 
 -type log() :: eleveldb:db_ref().
 -type write_result() :: ok | {error, any()}.
 -type get_result() :: {ok, any()} | not_found | {error, any()}.
--type index_result() :: {ok, integer()} | {error, any()}.
+-type index_get() :: {ok, integer()} | {error, any()}.
+-type metadata_get() :: {ok, dict()} | {error, any()}.
 
 -spec new(Filename :: binary()) -> {ok, log()} | {error, any()}.
 new(Filename) ->
@@ -23,9 +33,9 @@ new(Filename) ->
       [{create_if_missing, true}]
      ).
 
--spec last_index(log()) -> index_result().
+-spec last_index(log()) -> index_get().
 last_index(Log) ->
-    get_term(Log, ?INDEX_KEY).
+    metadata(Log, ?INDEX_KEY).
 
 -spec append_entry(log(), Term :: integer(), Entry :: any()) -> ok | {error, any()}.
 append_entry(Log, Term, Entry) ->
@@ -37,10 +47,69 @@ append_entry(Log, Term, Entry) ->
 entry(Log, Index) ->
     get_term(Log, Index).
 
+-spec voted_for(log()) -> get_result().
+voted_for(Log) ->
+    metadata(Log, voted_for).
+
+-spec voted_for(log(), CandidateId :: any()) -> write_result().
+voted_for(Log, CandidateId) ->
+    metadata(Log, voted_for, CandidateId).
+
+-spec term(log()) -> get_result().
+term(Log) ->
+    metadata(Log, term).
+
+-spec term(log(), any()) -> write_result().
+term(Log, Term) ->
+    metadata(Log, term, Term).
+
 %%===================================================================
 %% Internal functions
 %%===================================================================
--spec next_index(log()) -> index_result().
+-spec metadata(log(), Key :: any()) -> get_result().
+metadata(Log, Key) ->
+    MD = get_metadata(Log),
+    get_metadata_value(MD, Key).
+
+-spec get_metadata(log()) -> metadata_get().
+get_metadata(Log) ->
+    % convert the not_found get_result() into a new {ok, dict()}
+    % so that we'll never have a not_found for get_metadata
+    case get_term(Log, ?METADATA_KEY) of
+        not_found ->
+            {ok, dict:new()};
+        R ->
+            R
+    end.
+
+-spec write_metadata(log(), dict()) -> write_result().
+write_metadata(Log, MD) ->
+    write_term(Log, ?METADATA_KEY, MD).
+
+-spec metadata(log(), Key :: any(), Value :: any()) -> write_result().
+metadata(Log, Key, Value) ->
+    case get_metadata(Log) of
+        {ok, MD} ->
+            MD2 = dict:store(MD, Key, Value),
+            write_metadata(Log, MD2);
+        E={error, _} ->
+            E
+    end.
+
+            
+-spec get_metadata_value(metadata_get(), Key :: any()) -> get_result().
+get_metadata_value(E={error,_}, _) ->
+    E;
+get_metadata_value({ok, MD}, Key) ->
+    case dict:find(Key, MD) of
+        error ->
+            not_found;
+        {ok, V} ->
+            {ok, V}
+    end.
+                                
+
+-spec next_index(log()) -> index_get().
 next_index(Log) ->
     case last_index(Log) of
 	{ok, LastIndex} ->
@@ -49,18 +118,18 @@ next_index(Log) ->
 	    E
     end.
 
--spec update_index(log(), index_result(), write_result()) -> write_result().
+-spec update_index(log(), index_get(), write_result()) -> write_result().
 update_index(Log, {ok, Index}, {ok, _}) ->
-    write_term(Log, ?INDEX_KEY, Index);
+    metadata(Log, ?INDEX_KEY, Index);
 update_index(_, E={error, _}, _) ->
-    % index_result() error case
+    % index_get() error case
     E;
 update_index(_, _, E={error, _}) ->
     % store_entry() error case
     E.
 
 
--spec store_entry(log(), index_result(), 
+-spec store_entry(log(), index_get(), 
 		  Term :: integer(), Entry :: any()) -> write_result().
 store_entry(Log, {ok, Index}, Term, Entry) ->
     write_term(Log, Index, {Term, Entry});
